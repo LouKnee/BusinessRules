c60cd16d7969f59a6f70ce086f64d50cef6dca9b
Neal Moran <nealmoran23@gmail.com>
2021-10-18 17:28:20 +0100

    Commission payment tests

--- BusinessRulesTests/BusinessRulesTests.cs
+++ BusinessRulesTests/BusinessRulesTests.cs
@@ -74,8 +74,24 @@ namespace BusinessRulesTests
         {
             Account userAccount = new Account();
             userAccount.ProcessOrderPayment(Order.OrderType.VIDEO_ORDER, "Learning To Ski");
-            Assert.That(userAccount.CurrentOrder.Content.Any(c => c.GetDetails() == "Packing slip"));   
+            Assert.That(userAccount.CurrentOrder.Content.Any(c => c.GetDetails() == "Packing slip"));
             Assert.That(((Order)userAccount.CurrentOrder).PackingSlip.FirstAidVideoAttched, Is.True);
+        }
+
+        [Test]
+        public void ProductCommisionPaymentTest()
+        {
+            Account userAccount = new Account();
+            userAccount.ProcessOrderPayment(Order.OrderType.ITEM_ORDER);
+            Assert.That(userAccount.CurrentOrder.Content.Any(c => c.GetDetails() == "Commission Payment"));
+        }
+
+        [Test]
+        public void BookCommisionPaymentTest()
+        {
+            Account userAccount = new Account();
+            userAccount.ProcessOrderPayment(Order.OrderType.BOOK_ORDER);
+            Assert.That(userAccount.CurrentOrder.Content.Any(c => c.GetDetails() == "Commission Payment"));
         }
     }
 }
